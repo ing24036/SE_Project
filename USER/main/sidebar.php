21042906c1ad4900006c05f15c80f746db250c8b
worawuthjp <53623243+worawuthjp@users.noreply.github.com>
2020-03-04 23:47:59 +0700

    USER

---
+++ USER/main/sidebar.php
@@ -0,0 +1,49 @@
+<ul class="nav">
+    <li class="nav-item ">
+        <a class="nav-link" href="../index.php">
+            <i class="material-icons">dashboard</i>
+            <p>หน้าหลัก</p>
+        </a>
+    </li>
+    <!--<li class="nav-item ">
+        <a class='nav-link collapsed' href='#' data-toggle='collapse' data-target='#link-2' aria-expanded='true'
+            aria-controls='link-2'>
+            <i class="material-icons">person</i>
+            <p>จัดการอุปกรณ์</p>
+        </a>
+        <div id='link-2' class='collapse' aria-labelledby='headingTwo' data-parent='#accordionSidebar'>
+            <div class=' py-2 collapse-inner rounded'
+                style='border-left: 2px solid white; border-radius: 0% !important;'>
+                <div class="background-color-sideber" id="accordionSidebar">
+                    <a class='nav-link' href='../view/name_equipment.php'>รายการอุปกรณ์</a>
+                    <a class='nav-link' href='../view/name_type_equipment.php'>หมวดหมู่อุปกรณ์</a>
+                </div>
+            </div>
+        </div>
+    </li>-->
+    <!--<li class="nav-item ">
+        <a class="nav-link" href="../view/teacher.php">
+            <i class="material-icons">content_paste</i>
+            <p>รายชื่ออาจารย์</p>
+        </a>
+    </li>-->
+    <li class="nav-item ">
+        <a class="nav-link" href="../view/circulation.php">
+            <i class="material-icons">library_books</i>
+            <p>การยืม-คืนอุปกรณ์</p>
+        </a>
+    </li>
+    <li class="nav-item ">
+        <a class="nav-link" href="../view/history_circulation.php">
+            <i class="material-icons">bubble_chart</i>
+            <p>ประวัติการยืม-คืนอุปกรณ์</p>
+        </a>
+    </li>
+    
+    <!-- <li class="nav-item active-pro ">
+          <a class="nav-link" href="./upgrade.html">
+              <i class="material-icons">unarchive</i>
+              <p>Upgrade to PRO</p>
+          </a>
+      </li> -->
+</ul>
